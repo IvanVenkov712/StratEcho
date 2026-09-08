5d6986e2c1350bde81ac222a524d299b9c3f95d7
IvanVenkov712 <ivan.venkov.developer@gmail.com>
2026-09-08 12:31:20 +0300

    Fixed incorrect unpacking

--- src/backtester/sizing/asset_allocation.py
+++ src/backtester/sizing/asset_allocation.py
@@ -8,8 +8,8 @@ class AssetAllocation:
     allocations: dict[str, float]
 
     def __post_init__(self):
-        if any(weight < 0 or weight > 1 or not isfinite(weight) for weight, _ in self.allocations.values()):
+        if any(weight < 0 or weight > 1 or not isfinite(weight) for weight in self.allocations.values()):
             raise ValueError("Allocation weights must be finite numbers in [0, 1]")
 
-        if sum(weight for weight, _ in self.allocations.values()) > 1:
+        if sum(weight for weight in self.allocations.values()) > 1:
             raise ValueError("Allocation weights cannot exceed 1")
